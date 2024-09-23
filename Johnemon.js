3349f3d222a9dcd32d0a6ed58b40e7bfda759232
Neogiciaa <chevalier.stephen@hotmail.com>
2024-09-23 10:49:12 +0200

    Changed arrays structure for student name parts - Created generateRandomName, getRandomNumber and generate catchPhrase methods

--- Johnemon.js
+++ Johnemon.js
@@ -1,45 +1,81 @@
-const students = [
-  'Oli', 'via',
-  'No', 'ra',
-  'Dia', 'na',
-  'Mo', 'hab',
-  'Ly', 'ne',
-  'Séba', 'stien',
-  'Crist', 'elle',
-  'Far', 'id',
-  'Jul', 'ien',
-  'Jos', 'ias',
-  'Benj', 'amin',
-  'Mat', 'teo',
-  'Re', 'da',
-  'Dona', 'tien',
-  'Ren', 'aud',
-  'Anto', 'ine',
-  'Vinci', 'anne',
-  'Stép', 'hen',
-  'Moha', 'med',
-  'Ha', 'kim',
-  'Pie', 'rre',
-  'Hu', 'go',
-  'Th', 'éo',
-  'Maxi', 'me',
+const firstStudentsPart = [
+  'Oli',
+  'No',
+  'Dia',
+  'Mo',
+  'Ly',
+  'Séba',
+  'Crist',
+  'Far',
+  'Jul',
+  'Jos',
+  'Benj',
+  'Mat',
+  'Re',
+  'Dona',
+  'Ren',
+  'Anto',
+  'Vinci',
+  'Stép',
+  'Moha',
+  'Ha',
+  'Pie',
+  'Hu',
+  'Th',
+  'Maxi',
+]
+
+const secondStudentsPart = [
+  'via',
+  'ra',
+  'na',
+  'hab',
+  'ne',
+  'stien',
+  'elle',
+  'id',
+  'ien',
+  'ias',
+  'amin',
+  'teo',
+  'da',
+  'tien',
+  'aud',
+  'ine',
+  'anne',
+  'hen',
+  'med',
+  'kim',
+  'rre',
+  'go',
+  'éo',
+  'me',
 ]
 
 class Johnemon {
   constructor() {
-
+    this.name = this.generateRandomName();
+    this.level = 1;
+    this.experienceMeter = 0;
+    this.attackRange = this.getRandomNumber(1, 8);
+    this.defenseRange = this.getRandomNumber(1, 3);
+    this.healthPool = this.getRandomNumber(10, 30);
+    this.catchPhrase = this.generateCatchPhrase();
   }
 
   generateRandomName() {
-
+    const firstStudentPart = firstStudentsPart[Math.floor(Math.random() * firstStudentsPart.length)];
+    const secondStudentPart = secondStudentsPart[Math.floor(Math.random() * secondStudentsPart.length)];
+    return `${firstStudentPart}${secondStudentPart}`;
   }
 
   getRandomNumber(min, max) {
-
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   generateCatchPhrase() {
-
+    const catchPhrases = ["Let's fight together !", "Johnemon go !", "Time to fight !"];
+    return catchPhrases[Math.floor(Math.random() * catchPhrases.length)];
   }
 
   attack(defender) {
@@ -59,4 +95,4 @@ class Johnemon {
   }
 }
 
-module.exports = Johnemon
+module.exports = Johnemon;
