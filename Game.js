15d292227a349ee94c2f76908591eaa277578599
Neogiciaa <chevalier.stephen@hotmail.com>
2024-09-23 10:05:33 +0200

    First commit

--- Game.js
+++ Game.js
@@ -1,38 +1,28 @@
 const readline = require('readline');
-const JohnemonMaster = require('./JohnemonMaster'); // Replace 'your_classes_filename' with the actual filename
+const JohnemonMaster = require('./JohnemonMaster');
 const Johnemon = require('./Johnemon')
 const JohnemonWorld = require ('./JohnemonWorld')
 const fs = require('fs');
-
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+function saveGameState() {
 
-
-function saveGameState(){
-  
 }
-
-
 
 function askForName() {
-  
-}
-
-function proposeFirstJohnemon(rl){
 
 }
 
-function startGame(){
-  
+function proposeFirstJohnemon(rl) {
+
 }
 
+function startGame() {
 
-
+}
 
 startGame()
-
-
