4740a62ac5707b79ec4f0398d8d5c0c065668302
Neogiciaa <chevalier.stephen@hotmail.com>
2024-09-25 21:40:43 +0200

    Added logic to manage Johnemons (rename - heal - revive - release)

--- JohnemonMaster.js
+++ JohnemonMaster.js
@@ -1,4 +1,3 @@
-
 class JohnemonMaster {
   constructor(name) {
     this.name = name;
@@ -10,24 +9,47 @@ class JohnemonMaster {
   }
 
   renameJohnemon(johnemon, newName) {
-    console.log("RENAME")
+    let previousName = johnemon.name;
+    johnemon.name = newName;
+    return { success: true, message: `[System] ${previousName} has been renamed to ${newName}.` };
   }
 
-  healJohnemon(previousHealthPool) {
-    console.log("HEAL")
+  healJohnemon(johnemon) {
+    if (this.healingItems <= 0) {
+      return { success: false, message: '[System] No healing items left.' };
+    }
+
+    if (johnemon.healthPool === johnemon.baseHealthPool) {
+      return { success: false, message: `[System] ${johnemon.name} is already at full health.` };
+    }
+
+    johnemon.healthPool = Math.min(johnemon.healthPool + 20, johnemon.baseHealthPool);
+    this.healingItems--;
+    return { success: true, message: `[System] ${johnemon.name} has been healed. HP: ${johnemon.healthPool}/${johnemon.baseHealthPool}. Remaining healing items: ${ this.healingItems }` };
   }
 
-  reviveJohnemon(player, johnemon) {
-    //  johnemon.healthPool = Pv de base ??
-      player.reviveHealingItems -= 1;
+  reviveJohnemon(johnemon) {
+    if (this.reviveItems > 0) {
+      if (johnemon.healthPool <= 0) {
+        johnemon.healthPool = johnemon.baseHealthPool;
+        this.reviveItems--;
+        return { "success": true, "message": `[System] ${johnemon.name} has been revived with ${johnemon.healthPool} HP. Remaining revive items: ${ this.reviveItems }`}
+      } else {
+        return { "success": false, "message": `[System] ${johnemon.name} is still alive !`}
+      }
+    } else {
+      return { success: false, message: "[System] No more revive item left. Buy some to the store or drop some with fights or sleep to revive all of your johnemon's"}
+    }
   }
 
   releaseJohnemon(johnemon) {
-    console.log("RELEASE")
-  }
-
-  showCollection() {
-
+    const index = this.johnemonCollection.indexOf(johnemon);
+    if (index !== -1) {
+      this.johnemonCollection.splice(index, 1);
+      return { success: true, message: `[System] ${johnemon.name} has been released from your collection. `};
+    } else {
+      return { success: false, message: "[System] Johnemon not found in your collection." };
+    }
   }
 }
 
